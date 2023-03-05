b544771c37e9c2f0622dd5f17287cfd0337c0beb
hilaryyates <hilryyates@gmail.com>
2023-03-05 16:55:54 -0500

    card dimensions

--- todo-list/src/TodoBox.jsx
+++ todo-list/src/TodoBox.jsx
@@ -4,7 +4,8 @@ import TodoItem from "./TodoItem";
 
 const TodoBox = () => {
   return (
-    <div className='bg-white'>
+    <div>
+      <div className='bg-white w-300 h-510'></div>
       <span className='text-todo font-extrabold'>4</span>
       <span className='text-todo font-semibold'>March</span>
       <span className='text-todo'>2023</span>
--- todo-list/tailwind.config.cjs
+++ todo-list/tailwind.config.cjs
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      width: {
+        300: "300px",
+      },
+      height: {
+        510: "510px",
+      },
       colors: {
         complete: "#D0D2DA",
         todo: "#606372",
